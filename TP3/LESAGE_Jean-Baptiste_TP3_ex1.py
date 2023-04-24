eb6745597807c41edd8f5360dcf94f158c68b4c8
XeelS <jbaptiste.ln@gmail.com>
2023-04-24 23:22:46 +0200

    More linting

--- TP3/LESAGE_Jean-Baptiste_TP3_ex1.py
+++ TP3/LESAGE_Jean-Baptiste_TP3_ex1.py
@@ -12,85 +12,85 @@ def import_mat(n):
     lines = f.readlines()
     f.close()
     
-    A = np.array([[float(cara) for cara in line.split(" ") if cara != ''] for line in lines[1:n+1]])
-    B = np.array([[float(cara)] for cara in lines[n+2].split(" ") if cara != ''])
-    X = np.array([[float(cara)] for cara in lines[n+4].split(" ") if cara != ''])
+    a = np.array([[float(cara) for cara in line.split(" ") if cara != ''] for line in lines[1:n+1]])
+    b = np.array([[float(cara)] for cara in lines[n+2].split(" ") if cara != ''])
+    x = np.array([[float(cara)] for cara in lines[n+4].split(" ") if cara != ''])
     
-    return A, B, X
+    return a, b, x
 
 
-def jacobi(A, B, x0=None):
-    n, _ = A.shape
-    e, N = epsilon, 0
-    X = [np.zeros((n, 1), float) if x0 is None else x0]
-    A_ = A.copy()
-    np.fill_diagonal(A_, 0)
-    D = np.diag(A)[:, np.newaxis]
-    while N < N_max and e >= epsilon:
-        X.append((B - A_.dot(X[-1]))/D)
-        e = np.linalg.norm(X[-1] - X[-2])
-        N += 1
-    return X
+def jacobi(a, b, x0=None):
+    n, _ = a.shape
+    e, iteration = epsilon, 0
+    x = [np.zeros((n, 1), float) if x0 is None else x0]
+    a_ = a.copy()
+    np.fill_diagonal(a_, 0)
+    D = np.diag(a)[:, np.newaxis]
+    while iteration < N_max and e >= epsilon:
+        x.append((b - a_.dot(x[-1])) / D)
+        e = np.linalg.norm(x[-1] - x[-2])
+        iteration += 1
+    return x
 
 
-def gauss_seidel(A, B, x0=None):
-    n, _ = A.shape
-    e, N = epsilon, 0
-    X = [np.zeros((n, 1), float) if x0 is None else x0]
-    while N < N_max and e >= epsilon:
-        X.append(X[-1].copy())
+def gauss_seidel(a, b, x0=None):
+    n, _ = a.shape
+    e, iteration = epsilon, 0
+    x = [np.zeros((n, 1), float) if x0 is None else x0]
+    while iteration < N_max and e >= epsilon:
+        x.append(x[-1].copy())
         for i in range(n):
             s = 0
             for j in range(i):
-                s += A[i, j]*X[-1][j]
+                s += a[i, j] * x[-1][j]
             for j in range(i+1, n):
-                s += A[i, j]*X[-2][j]
-            X[-1][i] = (B[i] - s)/A[i, i]
-        e = np.linalg.norm(X[-1] - X[-2])
-        N += 1
-    return X
+                s += a[i, j] * x[-2][j]
+            x[-1][i] = (b[i] - s) / a[i, i]
+        e = np.linalg.norm(x[-1] - x[-2])
+        iteration += 1
+    return x
 
 
-def relaxation(A, B, x0=None, w=1.2):
-    n, _ = A.shape
-    e, N = epsilon, 0
-    X = [np.zeros((n, 1), float) if x0 is None else x0]
-    while N < N_max and e >= epsilon:
-        X.append(X[-1].copy())
+def relaxation(a, b, x0=None, w=1.2):
+    n, _ = a.shape
+    e, iteration = epsilon, 0
+    x = [np.zeros((n, 1), float) if x0 is None else x0]
+    while iteration < N_max and e >= epsilon:
+        x.append(x[-1].copy())
         for i in range(n):
             s = 0
             for j in range(i):
-                s += A[i, j]*X[-1][j]
+                s += a[i, j] * x[-1][j]
             for j in range(i+1, n):
-                s += A[i, j]*X[-2][j]
-            X[-1][i] = (B[i] - s)/A[i, i]
-            X[-1][i] = X[-2][i] + w*(X[-1][i] - X[-2][i])
-        e = np.linalg.norm(X[-1] - X[-2])
-        N += 1
-    return X
+                s += a[i, j] * x[-2][j]
+            x[-1][i] = (b[i] - s) / a[i, i]
+            x[-1][i] = x[-2][i] + w*(x[-1][i] - x[-2][i])
+        e = np.linalg.norm(x[-1] - x[-2])
+        iteration += 1
+    return x
 
 
-def change_array(X):
-    XX = np.empty((2, X.shape[1]*2))
-    XX[:, ::2] = XX[:, 1::2] = X
-    return XX[0, 1:], XX[1, :-1]
+def change_array(x):
+    xx = np.empty((2, x.shape[1] * 2))
+    xx[:, ::2] = xx[:, 1::2] = x
+    return xx[0, 1:], xx[1, :-1]
 
 
-def aff(A, B, X, method=None):
-    n, _ = A.shape
+def aff(a, b, x, method=None):
+    n, _ = a.shape
     
-    print('\nFin de', method, 'en', len(X), 'itérations')
-    print('Justesse :', np.linalg.norm(A.dot(X[-1]) - B))
+    print('\nFin de', method, 'en', len(x), 'itérations')
+    print('Justesse :', np.linalg.norm(a.dot(x[-1]) - b))
     
     if n == 2:
         plt.figure()
         
-        S = np.linspace(-10, 15, 2)
-        plt.plot(S, (B[0] - A[0, 0]*S)/A[0, 1], 'tab:blue')
-        plt.plot(S, (B[1] - A[1, 0]*S)/A[1, 1], 'tab:blue')
+        s = np.linspace(-10, 15, 2)
+        plt.plot(s, (b[0] - a[0, 0] * s) / a[0, 1], 'tab:blue')
+        plt.plot(s, (b[1] - a[1, 0] * s) / a[1, 1], 'tab:blue')
         
-        X = change_array(np.array(X).T[0])
-        plt.plot(X[0], X[1], 'r.--')
+        x = change_array(np.array(x).T[0])
+        plt.plot(x[0], x[1], 'r.--')
         
         plt.title(method)
         plt.show()
@@ -102,13 +102,13 @@ def main():
     print('+' + '-'*51 + '+')
 
     n = int(input("Saisir la dimension du système : "))
-    A, B, X = import_mat(n)
+    a, b, x = import_mat(n)
 
     print('-'*14 + ' Importation du système ' + '-'*15)
 
-    print("\nA :\n", A, sep='')
-    print("\nB :\n", B, sep='')
-    print("\nX :\n", X, sep='')
+    print("\nA :\n", a, sep='')
+    print("\nB :\n", b, sep='')
+    print("\nX :\n", x, sep='')
     print()
 
     #%%
@@ -122,9 +122,9 @@ def main():
     else:
         x0 = None
 
-    aff(A, B, jacobi(A, B, x0), 'Jacobi')
-    aff(A, B, gauss_seidel(A, B, x0), 'Gauss-Seidel')
-    aff(A, B, relaxation(A, B, x0), 'Relaxation')
+    aff(a, b, jacobi(a, b, x0), 'Jacobi')
+    aff(a, b, gauss_seidel(a, b, x0), 'Gauss-Seidel')
+    aff(a, b, relaxation(a, b, x0), 'Relaxation')
 
 
 if __name__ == '__main__':
